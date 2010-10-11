0901961a3af00e79ee487985b29020b04cf2aabc
Josh Gachnang <Josh@PCsForEducation.com>
2010-10-10 22:12:00 -0500

    Re-enabled commenting, as it now works. Created a better user interface for posting and comments, made the comment/post character counter work.

--- webwork/views.py
+++ webwork/views.py
@@ -45,7 +45,7 @@ def new_post(request):
 def show_comments(request, post_id):
     #Return list of comments, pass Post object
     post = Post.objects.get(id=post_id)
-    return render_to_response('comment_list.html', {'post': post}, RequestContext(request))
+    return render_to_response('comment_list.html', {'post': post, 'next': '/comments/' + str(post.id)}, RequestContext(request))
     
 def like_post(request, post_id):
     like_dislike(request, post_id, True)
