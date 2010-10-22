5f99178dfb6db6a3308fe15859f384b1b23c6b1c
Josh Gachnang <Josh@PCsForEducation.com>
2010-10-22 00:10:53 -0500

    Made top likes and top dislikes global through context processor.

--- context_processors.py
+++ context_processors.py
@@ -1,4 +1,11 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-z
+from webwork.models import Post
+def getTopLikes(num):
+    return Post.objects.order_by('-likes')[0:num]
+def getTopDislikes(num):
+    return Post.objects.all().order_by('-dislikes')[0:num]
+  
+
 def mlio_settings(request):
     from django.conf import settings
     import time, datetime
@@ -7,4 +14,5 @@ def mlio_settings(request):
     return { 'media_url': settings.MEDIA_URL, 'like': settings.RENAME_LIKE_DISLIKE[0], \
     'dislike': settings.RENAME_LIKE_DISLIKE[1], 'google_analytics_key': \
     settings.GOOGLE_ANALYTICS_KEY, 'site_title': settings.SITE_TITLE, 'site_url': settings.SITE_URL, 'year': year, \
-    'stardate': stardate }
+    'stardate': stardate, 'top_likes': getTopLikes(settings.TOP_LIKES), 'top_dislikes': getTopDislikes(settings.TOP_DISLIKES), }
+    
